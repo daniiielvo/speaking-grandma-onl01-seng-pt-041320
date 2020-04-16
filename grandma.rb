fe1531a97d3f3a7bc79dc66e94d8ad36582bdeeb
Daniel Vo <daniiielvo@gmail.com>
2020-04-16 01:53:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,14 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
         return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase != phrase 
-      return "HUH?! SPEAK UP, SONNY!"
+   
+    elsif phrase.upcase == phrase 
+      return "NO, NOT SINCE 1938!"
     
    
     
     else
-      return "NO, NOT SINCE 1938!"
+      return "HUH?! SPEAK UP, SONNY!"
     
     end
 end
